d3954c70996783a73c63a76e2557d828e75f820e
Matt Grasberger <grasbergerm@gmail.com>
2016-07-07 16:51:09 -0400

    changed bash script

--- bash_script.sh
+++ bash_script.sh
@@ -2,14 +2,14 @@
 sudo /opt/lampp/lampp start
 
 # Write IP to file inside repo
-/sbin/ip addr | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > ip.txt
+/sbin/ip addr | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > ~/ZAP_2.5.0/ip.txt
 
 # Run script to change context to new IP
 python change_context.py
 
 # Generate the random string for the API key
-cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 > apikey.txt
-
+cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 > $WORKSPACE/apikey.txt
+cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1 > ~/ZAP_2.5.0/apikey.txt
 # Start ZAP
 cd ~/ZAP_2.5.0/
 ./zap.sh -daemon -config api.key=`cat apikey.txt` &
